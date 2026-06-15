b0e5839fdc5349ea09c05fcadad4f703be9de08b
sapana <sapananagar51@gmail.com>
2026-06-15 06:40:40 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package demo.src.main.java.com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Sapana";
+        
+    }
+
+
+}
